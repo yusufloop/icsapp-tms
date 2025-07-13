9e789021506fd259109e08cab63a1ff6bf9e225f
yusufloop <plupies120@gmail.com>
2025-07-13 16:40:03 +0800

    env

--- lib/supabase.tsx
+++ lib/supabase.tsx
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import { Platform } from 'react-native';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_KEY!;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables');
